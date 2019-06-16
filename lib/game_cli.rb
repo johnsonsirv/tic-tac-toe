755ea3c8f32d282a6703ee3241837ee09324dafd
Victor Johnson <johnsonsirv@gmail.com>
2019-06-17 00:43:18 +0200

    initial commit to game_cli

---
+++ lib/game_cli.rb
@@ -0,0 +1,8 @@
+
+module UserInterface
+  
+  def show_board(state)
+    puts "here"
+  end
+
+end
--- spec/game_cli_spec.rb
+++ spec/game_cli_spec.rb
@@ -1,1 +1,27 @@
 require './lib/game_cli'
+
+#configure module for ui tests
+RSpec.configure do |c|
+  c.include UserInterface, :ui_tests => :ui
+end
+
+#game CLI
+RSpec.describe UserInterface do
+  let (:empty_state) {[" "]}
+  let (:game_won_state) {[""]}
+  let (:game_draw_state) {[""]}
+  let (:board_full_state) {[""]}
+  
+  describe '#display_board in different Game Board situations', :ui_tests => :ui do
+    it 'prints a blank board when the board state is empty' do
+      output = capture_puts{ show_board(empty_state) }
+      rows = output.split("\n")
+      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("   |   |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("   |   |   ")
+    end
+  end
+
+end
