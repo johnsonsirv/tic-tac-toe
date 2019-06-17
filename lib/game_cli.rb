db1d06f98d93a812d587077e2e57ac8f0d23cff6
Victor Johnson <johnsonsirv@gmail.com>
2019-06-17 03:27:53 +0200

    added logic to show_board on cli

--- lib/game_cli.rb
+++ lib/game_cli.rb
@@ -1,9 +1,18 @@
-
+# frozen_string_literal: true
 module UserInterface
   
   def show_board(state)
+    board = ""
+    grid_board = board.dup
+    row_1 = " #{state[0]} | #{state[1]} | #{state[2]} \n"
+    line = "-----------\n"
+    row_2 = " #{state[3]} | #{state[4]} | #{state[5]} \n"
+    row_3 = " #{state[6]} | #{state[7]} | #{state[8]} \n"
+    grid_board << row_1 << line
+    grid_board << row_2 << line
+    grid_board << row_3 << line
     
-    
+    puts grid_board
   end
 
 end
--- spec/game_cli_spec.rb
+++ spec/game_cli_spec.rb
@@ -7,20 +7,38 @@ end
 
 #game CLI
 RSpec.describe UserInterface do
-  let (:empty_state) {[" "]}
-  let (:game_won_state) {[""]}
+  let (:empty_state) {[""]*9}
+  let (:game_won_state) {["O"]*3}
   let (:game_draw_state) {[""]}
-  let (:board_full_state) {[""]}
+  let (:board_full_state) {["X","O","O","O","X","X","O","X","O"]}
   
   describe '#display_board in different Game Board situations', :ui_tests => :ui do
     it 'prints a blank board when the board state is empty' do
       output = capture_puts{ show_board(empty_state) }
       rows = output.split("\n")
-      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[0]).to eq("  |  |  ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   |   |   ")
+      expect(rows[2]).to eq("  |  |  ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq("   |   |   ")
+      expect(rows[4]).to eq("  |  |  ")
+    end
+    it 'prints a won board when board state is won' do
+      output = capture_puts{ show_board(game_won_state) }
+      rows = output.split("\n")
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("  |  |  ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("  |  |  ")
+    end
+    it 'prints a full board when the board state is full' do
+      output = capture_puts{ show_board(board_full_state) }
+      rows = output.split("\n")
+      expect(rows[0]).to eq(" X | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | X | O ")
     end
   end
 
