ed3e53f09d43a76e012f214be8077a4e9bcd1f0f
Victor Johnson <johnsonsirv@gmail.com>
2019-06-18 12:40:49 +0200

    game to decide first player randomly

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,18 @@
-require './lib/game_cli'
+# require './lib/game_cli'
 
 class Game
-  include UserInterface
-  
+  # include UserInterface
+  @@game_symbols = ["X","O"]
   attr_accessor :player_one, :player_two, :board
   
-  def initialize(symbol)
-  
+  def initialize(player_one, player_two, board)
+    @player_one = player_one
+    @player_two = player_two
+    @board = board
   end
 
-  
+  def choose_first_player_symbol
+    @@game_symbols[rand(2)]
+  end
   
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,26 +1,29 @@
 require './lib/game'
-
+require './lib/board'
+require './lib/player'
 
 
 # Game Controller
 RSpec.describe Game do
   before do 
+    @player_one = Player.new("X")
+    @player_two = Player.new("O")
     @board = Board.new
-   @game = Game.new("X","Y", @board)
+    @game = Game.new(@player_one, @player_two, @board)
   end
   
   describe '#game controller' do
     context '#game start' do
       it 'decides first player symbol' do
         first_player_symb = @game.choose_first_player_symbol
-        expect(@board.sate.include?first_player_symb).to eql(false)
+        expect(@board.state.include?(first_player_symb)).to eql(false)
+      end
+      it 'passes game turn between player 1 and player 2' do
+        
       end
     end
     context '#game in play' do
       it 'places the player\'s move on the board if position is free' do
-        
-      end
-      it 'passes game turn between player 1 and player 2' do
         
       end
       it 'returns gameover when there is a winnner and board is not full' do
