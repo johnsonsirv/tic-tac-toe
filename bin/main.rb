d85b8ed2d6d77201b2c437a1de0f727cddbbc763
Victor Johnson <johnsonsirv@gmail.com>
2019-06-19 13:07:16 +0200

    created main program control in the main.rb

---
+++ bin/main.rb
@@ -0,0 +1,13 @@
+require '../lib/game'
+require '../lib/board'
+require '../lib/player'
+require '../lib/game_cli'
+
+
+
+player_1 = Player.new("Victor", "X")
+player_2 = Player.new("Ojohny", "O")
+board = Board.new
+game = Game.new(player_1, player_2, board)
+
+game.start
--- test_game.rb
+++ test_game.rb
@@ -1,18 +1,3 @@
-# start
-# It decides player turn (random chooses the first player - symbol)
-
-# in play
-# It asks player to play
-# It places move on board if position is free
-# it decides gameover? when winner, board is full
-
-# gameover
-# it decides winner baased on win combinations
-
-# classs = {
-#   win: [ [1,2,3],[4,5,6],[7,8,9] ]
-# }
-
 # require './lib/game'
 # require './lib/board'
 # require './lib/player'
